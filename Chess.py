bdb9eacf3d901ef2f4fe7af1b8da9e082b6d64e4
mrM0nk <319WaEmoNK13>
2019-04-08 00:34:23 +0300

    добавлено два списка фигур по цвету, рефикторинг кода отрисовки шахмат,

--- Chess.py
+++ Chess.py
@@ -2,7 +2,7 @@ import gui
 
 
 if __name__ == "__main__":
-    gui.инициализация_интерфейса(info_panel=True, ask_for_change_settings=True, animation_delay=0.1)
+    gui.инициализация_интерфейса(info_panel=True, ask_for_change_settings=False, animation_delay=0.1)
 
     gui.отрисовка_шахмат()
 
