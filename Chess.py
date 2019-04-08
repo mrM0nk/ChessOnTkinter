b338419e79dbb1dbb6d8fa4ec900b1e240e5b476
mrM0nk <319WaEmoNK13>
2019-04-08 23:31:51 +0300

    создана кнопка Расчитать, которая очишает доску, рисует ее снова и выполняет заданную задачу скрипта создан ползунок регулировки скорости анимации и внесены соответствующие измения во все функции вынесена отрисовка доски в самостоятельную функцию

--- Chess.py
+++ Chess.py
@@ -1,9 +1,23 @@
 import gui
 
 
-if __name__ == "__main__":
-    gui.инициализация_интерфейса(info_panel=True, ask_for_change_settings=False, animation_delay=0.1)
+def btn_click(event):
+    try:
+        gui.кнопка_расчитать.unbind('<Button-1>')
+        gui.очистка_доски()
 
-    gui.отрисовка_шахмат()
+        gui.отрисовка_шахмат()
+
+        gui.кнопка_расчитать.bind('<Button-1>', btn_click)
+
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    gui.инициализация_интерфейса(info_panel=True, ask_for_change_settings=False)
+    gui.отрисовка_доски()
+
+    gui.кнопка_расчитать.bind('<Button-1>', btn_click)
 
     gui.окно.mainloop()
